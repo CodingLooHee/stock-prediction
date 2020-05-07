8bec7554e666c15e6dfffd42e8bdfc64475181bf
CodingLooHee <63284510+CodingLooHee@users.noreply.github.com>
2020-05-07 15:03:49 +0700

    Add model

--- trainer_1.py
+++ trainer_1.py
@@ -2,6 +2,11 @@ import utils
 import numpy as np
 import pandas as pd
 import pathlib
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+import tensorflow as tf
 
 PATH_CWD = pathlib.Path.cwd()
 PATH_STORAGE = PATH_CWD.joinpath('data')
@@ -26,4 +31,5 @@ data_y = data_y[32:, 4]
 print('Batched data x shape:', data_x.shape)
 print('Batched data y shape:', data_y.shape)
 
-
+model = utils.model.get_model.model_v1(input_shape=(32, 6))
+model.summary()
