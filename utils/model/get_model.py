0c617d6538d22860529aada9ea7d85ff06a10118
CodingLooHee <63284510+CodingLooHee@users.noreply.github.com>
2020-05-07 15:04:03 +0700

    Type annotation in get_model

--- utils/model/get_model.py
+++ utils/model/get_model.py
@@ -2,7 +2,7 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 from tensorflow import keras
 
-def model_v1(input_shape):
+def model_v1(input_shape: tuple):
     model = keras.Sequential()
     model.add(keras.layers.LSTM(64, input_shape=input_shape, activation='relu', return_sequences=True))
     model.add(keras.layers.Dropout(0.2))
