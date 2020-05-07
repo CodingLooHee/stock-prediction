f9667c05ed3f8e8ad26878232fdb16d9df304e5d
CodingLooHee <63284510+CodingLooHee@users.noreply.github.com>
2020-05-07 14:05:24 +0700

    Test in trainer_1.py

---
+++ trainer_1.py
@@ -0,0 +1,14 @@
+from utils import auto
+import numpy as np
+import pandas as pd
+import pathlib
+
+PATH_CWD = pathlib.Path.cwd()
+PATH_STORAGE = PATH_CWD.joinpath('data')
+
+PATH_DATA_MSFT = PATH_STORAGE.joinpath('MSFT.csv')
+
+
+raw = pd.read_csv(PATH_DATA_MSFT)
+raw = auto.data_auto(raw)
+print(raw)
