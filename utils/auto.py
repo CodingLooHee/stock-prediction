82232ce12d4116ed8ca32a94a2f63112ae7cdeaf
CodingLooHee <63284510+CodingLooHee@users.noreply.github.com>
2020-05-07 14:28:00 +0700

    More annotations

--- utils/auto.py
+++ utils/auto.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-def data_auto(data: pd.DataFrame):
+def data_auto(data: pd.DataFrame) -> np.ndarray:
     data.pop('Date')
     data_max = data.max(axis=0)
     data_min = data.min(axis=0)
