444ea6fbf9dba0b0f4a8ebe9e6f2abbd289707b4
CodingLooHee <63284510+CodingLooHee@users.noreply.github.com>
2020-05-07 14:58:54 +0700

    Add model_v1 in utils.model.get_model

---
+++ utils/model/get_model.py
@@ -0,0 +1,12 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+from tensorflow import keras
+
+def model_v1(input_shape):
+    model = keras.Sequential()
+    model.add(keras.layers.LSTM(64, input_shape=input_shape, activation='relu', return_sequences=True))
+    model.add(keras.layers.Dropout(0.2))
+    model.add(keras.layers.LSTM(32, activation='relu', return_sequences=False))
+    model.add(keras.layers.Dropout(0.2))
+    model.add(keras.layers.Dense(1, activation='linear'))
+    return model
