ffe9ac685bc46ac5afdf8d3d347834dba96be96c
CodingLooHee <63284510+CodingLooHee@users.noreply.github.com>
2020-05-07 14:22:41 +0700

    util.auto now auto normalize data

--- utils/auto.py
+++ utils/auto.py
@@ -2,4 +2,9 @@ import numpy as np
 
 def data_auto(data):
     data.pop('Date')
+    data_max = data.max(axis=0)
+    data_min = data.min(axis=0)
+
+    data = (data - data_min) / (data_max - data_min)
+
     return data
