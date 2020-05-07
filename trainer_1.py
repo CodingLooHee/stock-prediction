1de6c54002fb4d97b47e9d009e4a7cd7de18c58c
CodingLooHee <63284510+CodingLooHee@users.noreply.github.com>
2020-05-07 14:33:33 +0700

    Change package name

--- trainer_1.py
+++ trainer_1.py
@@ -10,7 +10,7 @@ PATH_DATA_MSFT = PATH_STORAGE.joinpath('MSFT.csv')
 
 
 raw = pd.read_csv(PATH_DATA_MSFT)
-raw = utils.datautils.auto.data_auto(raw)
+raw = utils.data_utils.auto.data_auto(raw)
 
 print('Data shape:', raw.shape)
 print('Data example:', raw[0])
