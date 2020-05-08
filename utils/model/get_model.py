f639fbbd7825b69ea3ea790a0897b0042448c6b5
CodingLooHee <63284510+CodingLooHee@users.noreply.github.com>
2020-05-08 11:32:26 +0700

    Use protected method instead of public in module

--- utils/model/get_model.py
+++ utils/model/get_model.py
@@ -1,13 +1,13 @@
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-from tensorflow import keras
+import os as _os
+_os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+from tensorflow import keras as _keras
 
 def model_v1(input_shape: tuple):
-    model = keras.Sequential()
-    model.add(keras.layers.LSTM(64, input_shape=input_shape, activation='relu', return_sequences=True))
-    model.add(keras.layers.Dropout(0.2))
-    model.add(keras.layers.LSTM(32, activation='relu', return_sequences=False))
-    model.add(keras.layers.Dropout(0.2))
-    model.add(keras.layers.Dense(1, activation='linear'))
-    model.compile(optimizer='adam', loss='mse')
+    model = _keras.Sequential()
+    model.add(_keras.layers.LSTM(64, input_shape=input_shape, activation='relu', return_sequences=True))
+    model.add(_keras.layers.Dropout(0.2))
+    model.add(_keras.layers.LSTM(32, activation='relu', return_sequences=False))
+    model.add(_keras.layers.Dropout(0.2))
+    model.add(_keras.layers.Dense(1, activation='linear'))
+    model.compile(optimizer='adam', loss='mse', metrics=['acc'])
     return model
