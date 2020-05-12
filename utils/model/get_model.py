644902eebfdd99f7f5cd0960f276806122629bc3
CodingLooHee <63284510+CodingLooHee@users.noreply.github.com>
2020-05-12 14:52:36 +0700

    Change model_v1

--- utils/model/get_model.py
+++ utils/model/get_model.py
@@ -5,9 +5,9 @@ from tensorflow import keras as _keras
 def model_v1(input_shape: tuple):
     model = _keras.Sequential()
     model.add(_keras.layers.LSTM(64, input_shape=input_shape, activation='relu', return_sequences=True))
-    model.add(_keras.layers.Dropout(0.2))
+    model.add(_keras.layers.Dropout(0.3))
     model.add(_keras.layers.LSTM(32, activation='relu', return_sequences=False))
-    model.add(_keras.layers.Dropout(0.2))
+    model.add(_keras.layers.Dropout(0.3))
     model.add(_keras.layers.Dense(1, activation='linear'))
     model.compile(optimizer='adam', loss='mse', metrics=['acc'])
     return model
