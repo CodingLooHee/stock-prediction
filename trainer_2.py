4771a345b199ef839bc8c6f02b4dc1b77fbb6168
CodingLooHee <63284510+CodingLooHee@users.noreply.github.com>
2020-05-14 13:35:35 +0700

    Add utils.env.wrapper.StockerV0's parameter tester

--- trainer_2.py
+++ trainer_2.py
@@ -5,12 +5,13 @@ import pathlib
 
 PATH_CWD = pathlib.Path.cwd()
 PATH_STORAGE = PATH_CWD.joinpath('data')
-PATH_DATA_MSFT = PATH_STORAGE.joinpath('MSFT.csv')
+PATH_DATA_AAPL = PATH_STORAGE.joinpath('AAPL.csv')
+print('Data path:', PATH_DATA_AAPL)
 
 
-data = pd.read_csv(PATH_DATA_MSFT)
+data = pd.read_csv(PATH_DATA_AAPL)
 data = utils.data.auto.data_auto(data)
+print('Data shape:', data.shape)
 
 
 env = utils.env.wrapper.StockerV0(data)
-
--- utils/env/wrapper.py
+++ utils/env/wrapper.py
@@ -1,7 +1,8 @@
 import numpy as _np
 
 class StockerV0:
-    def __init__(self, data):
+    def __init__(self, data, startup_range=[0, 100]):
+        # Test for data
         if not isinstance(data, _np.ndarray):
             raise TypeError('Data type must be numpy array')
         try:
@@ -9,5 +10,17 @@ class StockerV0:
         except:
             raise IndexError('Data must have 6 index following by: Open, High, Low, Close Adj, Close and Volume')
 
+        # Test for startup_range
+        if len(startup_range) != 2:
+            raise IndexError('startup_range must be 2 len list or tuple')
+        if not isinstance(startup_range[0], int) or not isinstance(startup_range[1], int):
+            raise TypeError('startup_range must be integer')
+        if startup_range[0] < 0 or startup_range[1] < 0:
+            raise ValueError('startup_range must be positive range')
+        if startup_range[0] > startup_range[1]:
+            raise ValueError('startup_range must be [min, max] format')
+
+        # If all parameter is in correct form. It's good to go.
         self._data = data
+        self.startup_range = startup_range
 
