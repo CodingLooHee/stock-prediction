25cddb41ce1312e5614636732c5648cedb8aac0a
CodingLooHee <63284510+CodingLooHee@users.noreply.github.com>
2020-05-14 13:02:26 +0700

    Add data loader

--- utils/env/wrapper.py
+++ utils/env/wrapper.py
@@ -1,5 +1,13 @@
-
+import numpy as _np
 
 class StockerV0:
     def __init__(self, data):
-        pass
+        if not isinstance(data, _np.ndarray):
+            raise TypeError('Data type must be numpy array')
+        try:
+            assert data.shape[1] == 6
+        except:
+            raise IndexError('Data must have 6 index following by: Open, High, Low, Close Adj, Close and Volume')
+
+        self._data = data
+
